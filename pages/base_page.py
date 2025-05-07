30c85a21fc4ad6e32a080ab457c633fce0d3f5d6
Лиана <vagapovaliana@mail.ru>
2025-05-07 15:15:30 +0300

    аше сообщение коммита

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,6 @@ import allure
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from locators.base_page_locators import BasePageLocators
-import time
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import ActionChains
 
@@ -13,18 +12,18 @@ class BasePage:
         self.timeout = timeout
 
     @allure.step('Открываем страницу: {url}')
-    def open(self, url, wait_seconds=2):
+    def open(self, url):
         self.driver.get(url)
-        if wait_seconds > 0:
-            time.sleep(wait_seconds)
 
-    @allure.step("Ожидание кликабельного элемента на странице")
-    def wait_element_to_be_clickable(self, locator):
-        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
 
-    @allure.step("Ожидание видимости элемента на странице")
-    def visibility_of_element(self, locator):
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+    @allure.step('Получаем текущий URL')
+    def get_current_url(self):
+        return self.driver.current_url
+    def wait_element_disappears(self, how, what):
+        try:
+            return WebDriverWait(self.driver, self.timeout).until(EC.invisibility_of_element_located((how, what)))
+        except TimeoutException:
+            raise TimeoutException(f'Modal window did not hide after {self.timeout} seconds')
 
     @allure.step('Получить текст')
     def get_text(self, locator):
@@ -37,19 +36,30 @@ class BasePage:
         except TimeoutException:
             raise TimeoutException(f'\nElement not clickable after {self.timeout} seconds')
 
-    def find_visability_element(self, how, what):
+    def find_visibility_element(self, how, what):
         try:
             return WebDriverWait(self.driver, self.timeout).until(EC.visibility_of_element_located((how, what)))
         except TimeoutException:
-            raise TimeoutException(f'\nElement not visability after {self.timeout} seconds')
+            raise TimeoutException(f'\nElement not visibility after {self.timeout} seconds')
 
-    def wait_element_disappears(self, how, what):
+    def presence_of_elements(self, how, what):
         try:
-            return WebDriverWait(self.driver, self.timeout).until(EC.invisibility_of_element_located((how, what)))
+            return WebDriverWait(self.driver, self.timeout).until(EC.presence_of_all_elements_located((how, what)))
         except TimeoutException:
-            raise TimeoutException(f'Modal window did not hide after {self.timeout} seconds')
+            raise TimeoutException(f'\nElements not present after {self.timeout} seconds')
 
 
+    @allure.step("Ожидание кликабельного элемента на странице")
+    def wait_element_to_be_clickable(self, how, what):
+        return self.find_clickable_element(how, what)
+
+    @allure.step("Ожидание видимости элемента на странице")
+    def visibility_of_element(self, how, what):
+        return self.find_visibility_element(how, what)
+
+    @allure.step('Нажимаем на заказ')
+    def click_button_order_feed(self):
+        self.find_clickable_element(*BasePageLocators.BUTTON_ORDER_FEED).click()
 
     @allure.step('Переместиться к элементу и кликнуть')
     def move_to_element_and_click(self, locator):
@@ -59,30 +69,15 @@ class BasePage:
 
     @allure.step('Нажимаем на кнопку "Личный кабинет" на главной странице')
     def click_button_personal_account(self):
-        WebDriverWait(self.driver,  10).until(
-             EC.element_to_be_clickable(BasePageLocators.BUTTON_PERSONAL_ACCOUNT)
-        ).click()
-
+        self.find_clickable_element(*BasePageLocators.BUTTON_PERSONAL_ACCOUNT).click()
 
     @allure.step('Найти элемент на странице')
     def find_element_on_page(self, locator):
         return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
 
     @allure.step('Найти элемент и кликнуть по нему')
-    def find_element_and_click(self, locator):
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
-        return self.find_element_on_page(locator).click()
-
-    @allure.step('Нажимаем на кнопку "Конструктор" на главной странице')
-    def click_button_link_constructor(self):
-        WebDriverWait(self.driver,  10).until(
-            EC.element_to_be_clickable(BasePageLocators.BUTTON_CONSTRUCTOR)
-        ).click()
-
-
-    @allure.step('Нажимаем на кнопку "Лента заказов" на главной странице')
-    def click_button_order_feed(self):
-        self.find_element_and_click(BasePageLocators.BUTTON_ORDER_FEED)
+    def find_element_and_click(self, how, what):
+        self.find_clickable_element(how, what).click()
 
 
     def text_present_in_element(self, how, what, text):
@@ -93,15 +88,23 @@ class BasePage:
 
 
     @allure.step('Перетащить элемент')
-    def drag_and_drop_to_element(self, from_locator, to_locator):
-        draggable = self.driver.find_element(*from_locator)
-        droppable = self.driver.find_element(*to_locator)
-        action_chains = ActionChains(self.driver)
-        action_chains.drag_and_drop(draggable, droppable).perform()
-
+    def drag_and_drop(self, how_s, what_s, how_t, what_t):
+        source = self.find_clickable_element(how_s, what_s)
+        target = self.find_visibility_element(how_t, what_t)
+        actions = ActionChains(self.driver)
+        actions.drag_and_drop(source, target).perform()
 
     @allure.step('Переместиться к элементу и кликнуть')
     def move_to_element_and_click(self, locator):
         element = self.driver.find_element(*locator)
         actions = ActionChains(self.driver)
         actions.move_to_element(element).click().perform()
+
+
+    @allure.step('Нажимаем на кнопку "Конструктор" на главной странице')
+    def click_button_link_constructor(self):
+        self.find_clickable_element(*BasePageLocators.BUTTON_CONSTRUCTOR).click()
+
+    @allure.step('Нажимаем на кнопку "Лента заказов" на главной странице')
+    def click_button_order_feed(self):
+        self.find_element_and_click(*BasePageLocators.BUTTON_ORDER_FEED)
--- pages/password_recovery_page.py
+++ pages/password_recovery_page.py
@@ -1,10 +1,7 @@
 import allure
-
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from locators.password_recovery_page_locators import PasswordRecoveryPageLocators
 from pages.base_page import BasePage
-import time
+from data import LoginData
 
 class PasswordRecoveryPage(BasePage):
     def __init__(self, driver):
@@ -13,47 +10,36 @@ class PasswordRecoveryPage(BasePage):
 
     @allure.step('Проверяем наличие кнопки "Восстановить" и возвращаем текущий URL')
     def find_button_restore_return_url(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(PasswordRecoveryPageLocators.BUTTON_RECOVER))
-        return self.driver.current_url
+        self.find_clickable_element(*PasswordRecoveryPageLocators.BUTTON_RECOVER)
+        return self.get_current_url()
 
 
     @allure.step('Вводим email')
-    def click_enter_email(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(PasswordRecoveryPageLocators.EMAIL_INPUT)
-        ).click()
-        time.sleep(3)
-        self.driver.find_element(*PasswordRecoveryPageLocators.BUTTON_RECOVER).send_keys('dsdsds@mail.ru')
+    def click_enter_email(self,email=LoginData.DEFAULT_USER.get('email')):
+        self.find_clickable_element(*PasswordRecoveryPageLocators.EMAIL_INPUT).send_keys(email)
+
 
     @allure.step('Нажимаем на кнопку "Восстановить" в форме "Восстановление пароля"')
     def click_restore(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(PasswordRecoveryPageLocators.BUTTON_RECOVER)
-        ).click()
+        self.find_clickable_element(*PasswordRecoveryPageLocators.BUTTON_RECOVER).click()
+
 
     @allure.step('Проверяем наличие кнопки "Сохранить" и возвращаем текущий URL')
     def find_button_save_return_url(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(PasswordRecoveryPageLocators.BUTTON_SAVE))
-        return self.driver.current_url
+        self.find_clickable_element(*PasswordRecoveryPageLocators.BUTTON_SAVE)
+        return self.get_current_url()
 
     @allure.step('Вводим пароль')
-    def click_enter_password(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(PasswordRecoveryPageLocators.PASSWORD_INPUT)
-        ).click()
-        time.sleep(3)
-        self.driver.find_element(*PasswordRecoveryPageLocators.PASSWORD_INPUT).send_keys('1a1s1s1d')
+    def click_enter_password(self,password=LoginData.DEFAULT_USER.get('password')):
+        self.find_clickable_element(*PasswordRecoveryPageLocators.PASSWORD_INPUT).send_keys(password)
+
 
     @allure.step('Нажимаем на кнопку показать/скрыть пароль')
     def click_button_show_password(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(PasswordRecoveryPageLocators.BUTTON_SHOW_PASSWORD)
-        ).click()
+        self.find_clickable_element(*PasswordRecoveryPageLocators.BUTTON_SHOW_PASSWORD).click()
+
 
     @allure.step('Проверка что поле "Пароль" активно')
     def password_field_is_active(self):
-        return WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(PasswordRecoveryPageLocators.ACTIVE_INPUT_PASSWORD)
-        ).is_displayed()
+        return self.find_visibility_element(*PasswordRecoveryPageLocators.ACTIVE_INPUT_PASSWORD).is_displayed()
+
